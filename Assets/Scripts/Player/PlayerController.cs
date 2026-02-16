ceb5957a3c36c5ff93be1c215daf5b8b538701d5
kseksha <malovsashalove@mail.ru>
2026-02-16 21:15:29 +0300

    Dash System

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,18 +5,23 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
-    
+    [SerializeField] private float dashSpeed = 8f;
+    [SerializeField] private float dashTime = 2f;
     private PlayerInputAction playerInputAction;
     private CharacterController characterController;
     private Vector2 moveInput;
     private Vector3 movementDirection;
+    private float currentSpeed;
+    private bool isDashing;
+    private float dashTimer;
+    
 
     private void OnEnable()
     {
         playerInputAction = new PlayerInputAction();
         playerInputAction.Enable();
         
-        playerInputAction.Player.Somersault.performed += context => DashHandler();
+        playerInputAction.Player.Dash.performed += context => DashHandler();
         playerInputAction.Player.Movement.performed += context => MovementPerformedHandler(context);
         playerInputAction.Player.Movement.canceled += context => MovementCanceledHandler();
     }
@@ -25,30 +29,47 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
+        currentSpeed = speed;
     }
 
     private void Update()
     {
         movementDirection = new Vector3(moveInput.x, 0, moveInput.y);
 
+        if (isDashing)
+        {
+            currentSpeed = dashSpeed;
+            dashTimer -= Time.deltaTime;
+            if (dashTimer <= 0)
+            {
+                isDashing = false;
+                currentSpeed = speed;
+            }
+        }
+        
         if (movementDirection.magnitude > 0)
         {
-            characterController.Move(movementDirection * speed * Time.deltaTime);
+            characterController.Move(movementDirection * currentSpeed * Time.deltaTime);
         }
     }
     
     private void OnDisable()
     {
-        playerInputAction.Player.Somersault.performed -= context => DashHandler();
+        playerInputAction.Player.Dash.performed -= context => DashHandler();
         playerInputAction.Player.Movement.performed -= context => MovementPerformedHandler(context);
         playerInputAction.Player.Movement.canceled -= context => MovementCanceledHandler();
         
         playerInputAction.Disable();
     }
-    
+
+
     private void DashHandler()
     {
-        // TODO: Implement dash logic in CK-9
+        if (!isDashing)
+        {
+            isDashing = true;
+            dashTimer = dashTime;
+        }
     }
     
     private void MovementPerformedHandler(InputAction.CallbackContext context)
